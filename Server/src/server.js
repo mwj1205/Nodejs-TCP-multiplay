34483aa4f4e66be4ffcfe0b181545019e3a00a8a
Minu Han <mwj1205@naver.com>
2024-10-24 20:15:48 +0900

    Add: 프로토콜 버퍼 선언
    - /server/src/protobuf 폴더에 프로토콜 버퍼 선언
    - handler id와 response code 지정

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,21 @@
 import net from 'net';
 import { HOST, PORT } from './constants/env.js';
 import { onConnection } from './events/onConnection.js';
-
+import initServer from './init/index.js';
 const server = net.createServer(onConnection);
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server is on ${HOST}:${PORT}`);
-});
+// 서버 시동
+const StartServer = async () => {
+  try {
+    // 서버 초기화
+    await initServer();
+    server.listen(PORT, HOST, () => {
+      console.log(`Server is on ${HOST}:${PORT}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+StartServer();
