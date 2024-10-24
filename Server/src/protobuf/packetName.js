7ce9b689450b0edf5454ee7df80a3047e388393f
Minu Han <mwj1205@naver.com>
2024-10-24 20:48:34 +0900

    Add: 패킷 네임 매핑 추가
    - 프로토 버퍼 파일을 불러올 때 편의성을 위해 패킷 네임을 매핑하는 파일 추가

---
+++ Server/src/protobuf/packetName.js
@@ -0,0 +1,18 @@
+// 편하기 매핑하기 위해 사용
+export const packetName = {
+  common: {
+    Packet: 'common.Packet',
+  },
+  initial: {
+    InitialPayload: 'initial.InitialPayload',
+  },
+  game: {
+    LocationUpdatePayload: 'game.LocationUpdatePayload',
+  },
+  response: {
+    Response: 'response.Response',
+  },
+  gameNotification: {
+    LocationUpdate: 'gameNotification.LocationUpdate',
+  },
+};
