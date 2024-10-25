bed9bfea6baecf31c1f547227162370ae73dbbbb
Minu Han <mwj1205@naver.com>
2024-10-25 11:09:48 +0900

    Add: 패킷 파싱
    - 클라이언트로 부터 data를 수신하면 onData에서 처리
    - 수신한 데이터를 packetParser에서 읽을 수 있도록 파싱

--- Server/src/constants/handlerIds.js
+++ Server/src/constants/handlerIds.js
@@ -1,6 +1,6 @@
 export const RESPONSE_SUCCESS_CODE = 0;
 
 export const HANDLER_IDS = {
-  INIT: 0,
+  INITIAL: 0,
   LOCATION_UPDATE: 2,
 };
--- Server/src/events/onConnection.js
+++ Server/src/events/onConnection.js
@@ -5,6 +5,10 @@ import { onError } from './onError.js';
 export const onConnection = (socket) => {
   console.log(`Client connected from :${socket.remoteAddress}:${socket.remotePort}`);
 
+  // 클라이언트마다 하나의 버퍼 유지
+  // 이 버퍼에 들어온 데이터를 저장할 것임
+  socket.buffer = Buffer.alloc(0);
+
   socket.on('data', onData);
   socket.on('end', onEnd);
   socket.on('error', onError);
