fc8859f92b077c185ff28a4c67f0a3a3c663c235
Minu Han <mwj1205@naver.com>
2024-10-28 20:46:48 +0900

    Update: game_db 스키마 수정
    - id와 device_id를 같이 저장할 필요성이 없다고 판단
    - id 부분 삭제
    - primary key는 device_id로 위임

--- Server/src/db/sql/game_db.sql
+++ Server/src/db/sql/game_db.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_coordinate
 (
-    id         VARCHAR(36) PRIMARY KEY,
-    device_id  VARCHAR(255) UNIQUE NOT NULL,
+    device_id  VARCHAR(255) PRIMARY KEY,
     x          DOUBLE,
     y          DOUBLE,
     end_time   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
