22094d83357ab532db04e3869c67b359d520e44c
Minu Han <mwj1205@naver.com>
2024-10-28 19:00:04 +0900

    Add: 유저 접속 종료 로직 추가
    - 유저가 접속을 종료하면 처리하는 로직 추가
    - 유저 세션에서 유저 제거
    - 유저가 참가하고 있던 게임 세션의 유저 목록에서 유저 제거
    - 유저가 접속을 종료하여 게임의 인원이 0명이 되면 해당 게임세션 삭제

--- Server/src/events/onError.js
+++ Server/src/events/onError.js
@@ -1,3 +1,20 @@
 export const onError = (socket) => (err) => {
   console.error('Socket error:', err);
+
+  try {
+    const user = getUserBySocket(socket);
+    // 유저가 게임에 참여중이었다면 제거
+    if (user) {
+      if (user.gameId) {
+        const gameSession = getGameSession(user.gameId);
+        if (game) {
+          gameSession.removeUser(user.id);
+        }
+      }
+    }
+
+    removeUser(socket);
+  } catch (err) {
+    handlerError(socket, err);
+  }
 };
