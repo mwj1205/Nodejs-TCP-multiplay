bc02bb737b60fc421940372862c13ea05bcd1ff6
Minu Han <mwj1205@naver.com>
2024-10-31 15:04:16 +0900

    Add: 추측항법 추가
    - 게임 세션 내에서 최대 latency를 기준으로 추측항법 적용
    - 클라이언트와 서버 사이의 경과 시간을 계산
    - 경과 시간과 속도를 곱해 이동 거리 계산

--- Server/src/classes/models/game.class.js
+++ Server/src/classes/models/game.class.js
@@ -20,7 +20,7 @@ class Game {
     this.users.push(user);
     user.setGameId(this.id);
     // 1초마다 user의 ping 메서드 호출
-    this.latencyManager.addPlayer(user.id, user.ping.bind(user), 1000);
+    this.latencyManager.addUser(user.id, user.ping.bind(user), 1000);
   }
 
   // 게임에 접속한 유저 검색
@@ -34,7 +34,7 @@ class Game {
     user.leaveGame();
     this.users = this.users.filter((user) => user.id !== userId);
     // 핑 interval 제거
-    this.latency.removeUser(userId);
+    this.latencyManager.removeUser(userId);
     // 유저 제거 후 게임에 남은 유저가 없다면 게임 세션 삭제
     if (this.users.length <= 0 && this.id !== GAME_FIRST_SESSION_ID) {
       this.latencyManager.clearAll();
@@ -53,13 +53,25 @@ class Game {
 
   // 본인을 제외한 게임 내의 모든 유저의 위치 정보
   getOtherLocation(userId) {
+    const maxLatency = this.getMaxLatency();
+
     const locationData = this.users
       .filter((user) => user.id !== userId)
       .map((user) => {
-        const { x, y } = user.getPosition();
+        const { x, y } = user.calculatePosition(maxLatency);
         return { id: user.id, playerId: user.playerId, x, y };
       });
+    console.log('locationData: ', locationData);
     return locationData;
+  }
+
+  // 게임의 유저 중 최대 latency 찾기
+  getMaxLatency() {
+    let maxLatency = 0;
+    this.users.forEach((user) => {
+      maxLatency = Math.max(maxLatency, user.latency);
+    });
+    return maxLatency;
   }
 }
 
